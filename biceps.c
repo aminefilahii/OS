099c3915d1eaf24f75b77516db5e00c908c554e2
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4025-linux.ad.polytech.sorbonne-universite.fr>
2026-03-26 16:04:13 +0100

    version 0.1

--- biceps.c
+++ biceps.c
@@ -6,53 +6,6 @@
 
 #define HOSTNAME_SIZE 256
 
-// var globales pour stocker les mots de la commande
-static char **Mots;
-static int NMots;
-// fnct pour copier une chaine, On reserve la memoire avec malloc, copie le contenu de la chaine puis renvoie cette nouvelle chaine
-char *copyString(char *s)
-{
-    char *copy = malloc(strlen(s) + 1);
-    if (copy == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    strcpy(copy, s);
-    return copy;
-}
-// fnct qui analyse la commande et la decoupe en mots
-int analyseCom(char *ligne)
-{
-    char *tmp = copyString(ligne); // copie car strsep modifie la chaine
-    char *save = tmp; // on garde l'adresse pour free
-    char *token;
-    int i = 0;
-    Mots = malloc(100 * sizeof(char *)); // table de mots (max 100)
-    if (Mots == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    token = strsep(&tmp, " \t\n");
-    while (token != NULL)
-    {
-        if (strlen(token) > 0) {
-            Mots[i++] = copyString(token);
-        }
-        token = strsep(&tmp, " \t\n");
-    }
-    NMots = i;
-    free(save); // liberation de la copie
-    return NMots;
-}
-
-// fonction pour liberer la memoire des mots
-void freeMots()
-{
-    for (int i = 0; i < NMots; i++) {
-        free(Mots[i]);
-    }
-    free(Mots);
-}
 int main(void)
 {
     char hostname[HOSTNAME_SIZE];
@@ -88,19 +41,11 @@ int main(void)
     snprintf(prompt, taille_prompt, "%s@%s%c ", user, hostname, symbole_fin);
     while (1) {
         ligne = readline(prompt);
-
         if (ligne == NULL) {
             printf("\nfin\n");
             break;
         }
-        analyseCom(ligne); // analyse de la commande
-        if (NMots > 0) {
-            printf("commande : %s\n", Mots[0]); // premier mot = commande
-            for (int i = 1; i < NMots; i++) {
-                printf("argument %d : %s\n", i, Mots[i]);
-            }
-        }
-        freeMots(); // liberation memoire
+        printf("%s\n", ligne);
         free(ligne);
     }
     free(prompt);
